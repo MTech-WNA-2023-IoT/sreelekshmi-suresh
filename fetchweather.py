cc095162d8a88bcd57111c80ed7eb464adf896c9
vibhalekshmi <sreelekshmisuresh94@gmail.com>
2023-05-29 10:22:39 +0530

    Update fetchweather.py

--- fetchweather.py
+++ fetchweather.py
@@ -2,7 +2,7 @@ import json
 from urllib.request import urlopen
 #Create user account and obtain API key from https://www.weatherapi.com
 
-url = "https://www.weatherapi.com/api-explorer.aspx"
+url = "http://api.weatherapi.com/v1/current.json?key=e07cf783bb8842669da43922232905&q=London&aqi=no"
 
 api_page = urlopen(url)
 api=api_page.read()
