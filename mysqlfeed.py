301a12e02996e903023384f1e81c319fc2069578
vibhalekshmi <sreelekshmisuresh94@gmail.com>
2023-06-12 14:56:44 +0530

    Update mysqlfeed.py

--- mysqlfeed.py
+++ mysqlfeed.py
@@ -3,13 +3,13 @@
 #Import pymysql module library
 import pymysql
 #Create a connection to MySQL Database 
-conn =pymysql.connect(database="databasename",user="user",password="password",host="localhost")
+conn =pymysql.connect(database="weatherdata",user="user",password="Pass",host="localhost")
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
-data={'name':'hello','age':10,'place':'kollam'}
+data={'id':'1','location':kerala,'temperature':'20','humidity':'30'}
 #Execute the SQL to write data to the database
-cur.execute("INSERT INTO <tablename>(name, age, place)VALUES(%(name)s,%(age)s,%(place)s);",data)
+cur.execute("INSERT INTO <atmosphere>(id, location, temperature,humidity)VALUES(%(id)s,%(location)s,%(temperature)s),%(humidity)s;",data)
 #Close the cursor
 cur.close()
 #Commit the data to the database
